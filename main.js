ce83807bc7a6debee65ce567235e12a1507de1f2
haroldZS <haroldzs1024@gmail.com>
2023-06-29 17:08:48 -0400

    Implemented shopping cart menu with javascript

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const toggleMenu = document.querySelector(".menu");
+const cartMenu = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
+const cartDetail = document.querySelector(".cart-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 toggleMenu.addEventListener("click", toggleMobileMenu);
+cartMenu.addEventListener("click", toggleAsideCart);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+  addInactive(cartDetail, mobileMenu);
 }
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
+  addInactive(cartDetail, desktopMenu);
+}
+
+function toggleAsideCart() {
+  cartDetail.classList.toggle("inactive");
+  addInactive(mobileMenu, desktopMenu);
+}
+
+function addInactive(...elements) {
+  elements.forEach(element => {
+    element.classList.add("inactive");
+  });
 }
